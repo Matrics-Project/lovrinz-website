c3ab256f40be7ff783324e6adc17827d504b1e3e
auranfata <auranfata456@gmail.com>
2024-12-06 12:17:56 +0700

    fronted

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+Route::inertia('/', 'Home');
 
 Route::inertia('/admin/dashboard', 'Dashboard');
 Route::inertia('/admin/login', 'Login');
+Route::inertia('/admin/form', 'Form');
+Route::inertia('/admin/FormEdit', 'FormEdit');
+
+Route::inertia('/guest/test', 'Test');
+
+Route::inertia('/guest/home', 'Home');
