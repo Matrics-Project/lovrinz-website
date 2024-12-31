25c66bbd6cbcfc9fb66524df6bc5c0e80f8065df
Hamadah <muhammadrozaan@gmail.com>
2024-12-31 08:06:34 +0700

    feat(frontend): Update dashboard

--- resources/js/Pages/Books/Dashboard.jsx
+++ resources/js/Pages/Books/Dashboard.jsx
@@ -49,7 +49,7 @@ export default function Dashboard({ books }) {
                                         <thead>
                                             <tr>
                                                 <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
-                                                    Title Book
+                                                    Title Books
                                                 </th>
                                                 <th className="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50">
                                                     Category
