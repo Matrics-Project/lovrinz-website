5206cdb303f69a0c027917d0ffc1d55240c7fe36
Hamadah <muhammadrozaan@gmail.com>
2025-01-08 11:22:38 +0700

    fix(component): fix wrong component name case

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Navbar from "../components/Navbar.jsx";
-import Footer from "../components/footer.jsx";
+import Navbar from "@/components/Navbar.jsx";
+import Footer from "@/components/Footer.jsx";
 
 const products = [
     {
